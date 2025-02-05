16fdea43d9b7a7324b505f8a36ff258e0d29f935
reduktr <reduktion303@pm.me>
2025-02-05 13:49:10 -0800

    remove suspend script

--- .config/hypr/scripts/suspend.sh
+++
@@ -1,5 +0,0 @@
-swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 330 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 10800 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
